d1f590bbbf194e13c102ef3b7ee6f87246692535
Mateusz Stępień <mateusz.stepien@interia.eu>
2018-05-19 03:23:14 +0200

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# arcade-game
+## ↘︎
+
+![SCREENSHOT](img/screenshot.png)
+
+In this Arcade Game you are a player and you need to stay away from your enemies - bugs. The goal of the player is to reach the water, without colliding into any one of the enemies. The player can move left, right, up and down. The enemies move in varying speeds on the paved block portion of the scene. Once a the player collides with an enemy, the game is reset and the player moves back to the start square. Once the player reaches the water the game is won.
+
+## How The Game Is Played
+
+1. Choose your character.
+2. Place your fingers on arrows located on your keyboard. 
+3. Analyse movement of your enemies.
+4. Build a strategy in your mind.
+5. Run!
+
+## Characters
+
+- BOY
+- PINKY
+- WITCH
+- CATGIRL
+- PRINCESS
+
+## What The Player Is Expect From The Game
+
+- Be happy - it's just a game, you have three lives to be a hero.
+- Be focused - the bugs are programmed, try to understand the algorithm behind them.
+- Be proud - if you finish the game without receving an alert: "GAME OVER", you are the best!
+
+## Hint
+
+Haste makes waste.
+
+## Resources
+
+* [Background](https://css-tricks.com/stripes-css/)
---
+++ js/app.js
@@ -0,0 +1,165 @@
+// Enemies our player must avoid
+
+var Enemy = function (row, spd, x) {
+    
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    
+    this.spd = spd;
+    this.x = x;
+    if (row === 1) {
+        this.y = 239;
+    } else if (row === 2) {
+        this.y = 156;
+    } else {
+        this.y = 73;
+    }
+    
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    //
+    
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+
+Enemy.prototype.update = function (dt) {
+    
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    
+    if (this.x > 460) {
+        this.x = -30;
+    } else {
+        this.x += (101 * dt * this.spd);
+    }
+    this.render();
+};
+
+// Draw the enemy on the screen, required method for game
+
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+var player = function () {
+    this.x = 200;
+    this.y = 405;
+    this.life = 3;
+    this.sprite = (choosePlayer)();
+}
+var choosePlayer = function () {
+    var str = "CHOOSE YOUR CHARACTER:\n \n1 BOY \n2 PINKY\n3 WITCH\n4 CATGIRL\n5 PRINCESS\n"
+    var char = prompt(str);
+    switch (char) {
+        case "1":
+            return "images/char-boy.png";
+            break;
+        case "2":
+            return 'images/char-pink-girl.png';
+            break;
+        case "3":
+            return 'images/char-horn-girl.png';
+            break;
+        case "4":
+            return 'images/char-cat-girl.png';
+            break;
+        case "5":
+            return 'images/char-princess-girl.png';
+            break;
+        default:
+            return "images/char-boy.png";
+            break;
+    }
+};
+var choosePlayerUI = function () {
+    context.save();
+    context.restore();
+};
+
+player.prototype.update = function (x, y) {
+    if (x !== undefined && y !== undefined) {
+        this.x = x;
+        this.y = y;
+    } else if (y === undefined && x !== undefined) {
+        this.x = x;
+    } else if (x === undefined && y !== undefined) {
+        this.y = y;
+    }
+
+    this.render();
+};
+player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+player.prototype.handleInput = function (key) {
+
+    if (key === 'left') {
+        if (this.x !== -2) {
+            this.x -= 101;
+        }
+    } else if (key === 'up') {
+        if (this.y !== -10) {
+            this.y -= 83;
+        }
+    } else if (key === 'down') {
+        if (this.y !== 405) {
+            this.y += 83;
+        }
+    } else if (key === 'right') {
+        if (this.x !== 402) {
+            this.x += 101;
+        }
+
+    }
+    console.log(this.x);
+    console.log(this.y);
+};
+
+player.prototype.reset = function () {
+    this.x = 200;
+    this.y = 405;
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+var enemy1 = new Enemy(1, 1.2, -10);
+var enemy2 = new Enemy(1, 3.1, -400);
+var enemy3 = new Enemy(2, 1.9, -20);
+var enemy4 = new Enemy(2, 1.3, -150);
+var enemy5 = new Enemy(3, 1.7, -300);
+var enemy6 = new Enemy(3, 3.5, -5);
+
+var allEnemies = [];
+allEnemies.push(enemy1);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
+allEnemies.push(enemy4);
+allEnemies.push(enemy5);
+allEnemies.push(enemy6);
+
+var player = new player();
+
+// This listens for key presses and sends the keys to your
+
+// Player.handleInput() method. You don't need to modify this.
+
+document.addEventListener('keyup', function (e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down',
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
